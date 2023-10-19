c2b9cd2d70c914c27197be5559c86c534272526f
taufique01 <taufiquehr@gmail.com>
2023-10-19 18:55:55 +0600

    add filtyering method in github

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import Navbar from "./layout/Navbar";
 import Search from "./layout/Search";
-import { Box } from '@mui/material';
+import { Box } from "@mui/material";
 
 function App() {
   const [searches, setSearches] = useState([]);
 
   const handleAddSearch = () => {
     setSearches([...searches, <Search key={searches.length} />]);
-  }
+  };
 
   return (
-      <>
+    <>
       <Navbar onAddSearch={handleAddSearch} />
-      <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-        {searches}
-      </Box>
-      </>
-  )
+      <Box>{searches}</Box>
+    </>
+  );
 }
-export default App 
+export default App;
--- frontend/src/layout/Search.jsx
+++ frontend/src/layout/Search.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   TextField,
   Select,
@@ -8,36 +8,36 @@ import {
   Box,
   Button,
   CircularProgress,
-} from '@mui/material';
+} from "@mui/material";
 
 const Search = () => {
-  const [field, setField] = useState('field1');
-  const [regex, setRegex] = useState('');
-  const [size, setSize] = useState('');
-  const [position, setPosition] = useState('');
+  const [field, setField] = useState("field1");
+  const [regex, setRegex] = useState("");
+  const [size, setSize] = useState("");
+  const [position, setPosition] = useState("");
   const [isSearching, setIsSearching] = useState(false);
 
   const jsonToCsv = (jsonData) => {
-    let csv = '';
+    let csv = "";
     let headers = Object.keys(jsonData[0]);
-    csv += headers.join(',') + '\n';
+    csv += headers.join(",") + "\n";
     jsonData.forEach(function (row) {
-        let data = headers.map(header => JSON.stringify(row[header])).join(','); 
-        csv += data + '\n';
+      let data = headers.map((header) => JSON.stringify(row[header])).join(",");
+      csv += data + "\n";
     });
     return csv;
-  }
+  };
 
   const downloadCsvfile = (data) => {
     let csvData = jsonToCsv(data);
-    let blob = new Blob([csvData], { type: 'text/csv' });
+    let blob = new Blob([csvData], { type: "text/csv" });
     let url = window.URL.createObjectURL(blob);
-    let a = document.createElement('a');
+    let a = document.createElement("a");
     a.href = url;
-    a.download = 'data.csv';
+    a.download = "data.csv";
     document.body.appendChild(a);
     a.click();
-    };
+  };
 
   const handleFieldChange = (event) => {
     setField(event.target.value);
@@ -59,32 +59,39 @@ const Search = () => {
     event.preventDefault();
     if (field && regex && size && position) {
       setIsSearching(true);
-        const url = "http://127.0.0.1:8000/pandas/?field=" + field + "&regex=" + regex + "&size=" + size + "&position=" + position;
-        fetch(url).then(
-            response => response.json()).then(data => {
-            downloadCsvfile(data['data']);
-            setIsSearching(false);
-        }
-        ).catch(error => {
-            alert(error);
-            setIsSearching(false);
-        }
-        );
+      const url =
+        "http://127.0.0.1:8000/pandas/?field=" +
+        field +
+        "&regex=" +
+        regex +
+        "&size=" +
+        size +
+        "&position=" +
+        position;
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          downloadCsvfile(data["data"]);
+          setIsSearching(false);
+        })
+        .catch((error) => {
+          alert(error);
+          setIsSearching(false);
+        });
     }
   };
 
   return (
     <Box
       sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        padding: '16px',
-        maxWidth: '300px',
-        margin: 'auto',
-        marginTop: '100px',
+        display: "flex",
+        flexDirection: "row",
+        padding: "16px",
+        margin: "auto",
+        marginTop: "100px",
       }}
     >
-      <FormControl sx={{ marginBottom: '16px' }}>
+      <FormControl sx={{ marginBottom: "16px" }}>
         <InputLabel id="field-label">Field</InputLabel>
         <Select
           labelId="field-label"
@@ -93,26 +100,25 @@ const Search = () => {
           label="Field"
           onChange={handleFieldChange}
         >
-        <MenuItem value="field1">Field 1</MenuItem>
-        <MenuItem value="field2">Field 2</MenuItem>
-        <MenuItem value="field3">Field 3</MenuItem>
-        <MenuItem value="field4">Field 4</MenuItem>
-        <MenuItem value="field5">Field 5</MenuItem>
-        <MenuItem value="field6">Field 6</MenuItem>
-        <MenuItem value="field7">Field 7</MenuItem>
-        <MenuItem value="field8">Field 8</MenuItem>
-        <MenuItem value="field9">Field 9</MenuItem>
-        <MenuItem value="field10">Field 10</MenuItem>
+          <MenuItem value="field1">Field 1</MenuItem>
+          <MenuItem value="field2">Field 2</MenuItem>
+          <MenuItem value="field3">Field 3</MenuItem>
+          <MenuItem value="field4">Field 4</MenuItem>
+          <MenuItem value="field5">Field 5</MenuItem>
+          <MenuItem value="field6">Field 6</MenuItem>
+          <MenuItem value="field7">Field 7</MenuItem>
+          <MenuItem value="field8">Field 8</MenuItem>
+          <MenuItem value="field9">Field 9</MenuItem>
+          <MenuItem value="field10">Field 10</MenuItem>
         </Select>
       </FormControl>
-      <TextField
-        id="regex-input"
-        label="Regex"
-        variant="outlined"
-        value={regex}
-        onChange={handleRegexChange}
-        sx={{ marginBottom: '16px' }}
-      />
+      <Select label="Filtering option">
+        {FilteringMethods.map((value, index) => (
+          <MenuItem value={value.method} key={index}>
+            {value.label}
+          </MenuItem>
+        ))}
+      </Select>
       <TextField
         id="size-input"
         label="Size"
@@ -120,29 +126,44 @@ const Search = () => {
         variant="outlined"
         value={size}
         onChange={handleSizeChange}
-        sx={{ marginBottom: '16px' }}
+        sx={{ marginBottom: "16px" }}
       />
-      <TextField
-        id="position-input"
-        label="Position"
-        variant="outlined"
-        value={position}
-        onChange={handlePositionChange}
-        sx={{ marginBottom: '16px' }}
-      />
-      <Button
-        variant="contained"
-        onClick={handleSubmit}
-        disabled={isSearching || !field || !regex || !size || !position}
-      >
-        {isSearching ? (
-          <CircularProgress sx={{ color: 'white' }} size={24} />
-        ) : (
-          'Search'
-        )}
-      </Button>
     </Box>
   );
 };
 
 export default Search;
+
+const FilteringMethods = [
+  { label: "Regex", method: "regex", inputs: ["value"] },
+
+  {
+    label: "Contains",
+    method: "contains",
+    inputs: ["value"],
+  },
+
+  {
+    label: "Not contains",
+    method: "not_contains",
+    inputs: ["value"],
+  },
+
+  {
+    label: "Starts with",
+    method: "starts_with",
+    inputs: ["value"],
+  },
+
+  {
+    label: "Ends with",
+    method: "ends_with",
+    inputs: ["value"],
+  },
+
+  {
+    label: "Exact",
+    method: "exact",
+    inputs: ["value"],
+  },
+];
