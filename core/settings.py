6566275ea8654ec74e0766cc1ad667610bb1bb58
taufique01 <taufiquehr@gmail.com>
2023-11-13 19:41:49 +0600

    make debug true

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-42)03m&y=2qf0!bts0#3)7$g6dweadj5@9_@o^l@4v5aiw#zg_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['uxlivinglab200112.pythonanywhere.com' ,'127.0.0.1']
 
